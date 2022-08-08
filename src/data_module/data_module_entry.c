88584b658b2a112881a2cdca72cfc635de42d956
Kwaltz Buck <kwaltzbu@se-b5.nsk.21-school.ru>
2022-08-08 12:26:11 +0700

    quest1

--- src/data_module/data_module_entry.c
+++ src/data_module/data_module_entry.c
@@ -1,14 +1,19 @@
-void main()
-{
-    double *data;
-    int n;
-    
-    //Don`t forget to allocate memory !
+#include <stdio.h>
 
-    input(data, n);
+#include "data_process.c"
 
-    if (normalization(data, n))
-        output(data, n);
-    else
-        printf("ERROR");  
+int main() {
+  double *data;
+  int n;
+
+  if (input(&data, &n)) {
+    int result = normalization(data, n);
+    if (result == 0) printf("ERROR");
+    if (result == 1) output(data, n);
+    if (result == 2) printf("n/a");
+    free(data);
+  } else {
+    printf("n/a");
+  }
+  return 0;
 }
--- src/data_module/data_process.c
+++ src/data_module/data_process.c
@@ -1,24 +1,68 @@
 #include "data_process.h"
+
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int normalization(double *data, int n)
-{
-    int result = 1;
-    double max_value = max(data, n);
-    double min_value = min(data, n);
-    double size = max_value - min_value;
+int normalization(double *data, int n) {
+  int result = 1;
+  double max_value = max(data, n);
+  double min_value = min(data, n);
+  double size = max_value - min_value;
+  if (size == 0) result = 2;
+  if (fabs(size) < EPS && size != 0) {
+    for (int i = 0; i < n; i++) {
+      data[i] = (data[i] - min_value) / size;
+    }
+  } else {
+    result = 0;
+  }
 
-    if(fabs(size) < EPS)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            data[i] = data[i] / size - min_value / size;
+  return result;
+}
+
+int input(double **data, int *n) {
+  int check = 1;
+  char c;
+  if (scanf("%d%c", n, &c) == 2 && c != '.') {
+    *data = malloc(*n * sizeof(int));
+    if (*data != NULL) {
+      for (int i = 0; i < *n; i++) {
+        double el;
+        if (scanf("%lf", &el)) {
+          (*data)[i] = el;
+        } else {
+          check = 0;
+          break;
         }
+      }
+    } else {
+      check = 0;
     }
+  } else {
+    check = 0;
+  }
+  return check;
+}
+double max(double *data, int n) {
+  int max = data[0];
+  for (int i = 0; i < n; i++) {
+    if (max < data[i]) max = data[i];
+  }
+  return max;
+}
+double min(double *data, int n) {
+  double min = data[0];
+  for (int i = 0; i < n; i++) {
+    if (min > data[i]) min = data[i];
+  }
+  return min;
+}
+void output(double *data, int n) {
+  for (int i = 0; i < n; i++) {
+    if (i != n - 1)
+      printf("%.2lf ", data[i]);
     else
-    {
-        result = 0;
-    }
-    
-    return result;
+      printf("%.2lf", data[i]);
+  }
 }
