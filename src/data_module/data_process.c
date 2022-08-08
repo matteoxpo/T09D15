5531084bf00ac22e4b54e328c7c852518b2d5e32
Kwaltz Buck <kwaltzbu@se-b5.nsk.21-school.ru>
2022-08-08 19:48:30 +0700

    some new

--- src/data_module/data_process.c
+++ src/data_module/data_process.c
@@ -13,9 +13,9 @@ int normalization(double *data, int n) {
   double min_value = min(data, n);
   double size = max_value - min_value;
   if (size == 0) result = 2;
-  if (fabs(size) < EPS && size != 0) {
+  if (fabs(size) > EPS && size != 0) {
     for (int i = 0; i < n; i++) {
-      data[i] = (data[i] - min_value) / size;
+      data[i] = data[i] / size - min_value / size;
     }
   } else {
     result = 0;
